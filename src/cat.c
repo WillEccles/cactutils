0c1835a000caaeaacd3b4cd2bf47be55275e3538
Will Eccles <ghostyx123@gmail.com>
2020-01-05 21:10:12 -0500

    cat from stdin obviously

--- src/cat.c
+++ src/cat.c
@@ -2,8 +2,17 @@
 
 int main(int argc, char** argv) {
     if (argc == 1) {
-        fprintf(stderr, "Please provide a file.\n");
-        return 1;
+        char c, l;
+        while (EOF != (c = getchar())) {
+            printf("%c", c);
+            l = c;
+        }
+
+        if (EOF == c && '\n' != l) {
+            printf("\n");
+        }
+
+        return 0;
     }
 
     FILE* infile = fopen(argv[1], "rb");
